f0401b47606aab3a95067dc227a4d52fab0f41d0
emma <marenziemma@gmail.com>
2025-06-19 13:58:05 -0300

    Mi aplicación proyecto Django, primer commit

--- blog/models.py
+++ blog/models.py
@@ -1,28 +1,28 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 class Publicacion(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
-    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     texto = models.TextField()
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
     fecha_publicacion = models.DateTimeField(blank=True, null=True)
-
+    
     def publicar(self):
         self.fecha_publicacion = timezone.now()
         self.save()
-
-    def _str_(self):
+        
+    def __str__(self):
         return self.titulo
-
-
-class ObraArte(models.Model):
+    
+class ObraArte (models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     descripcion = models.TextField()
     fecha_creacion = models.DateField()
-    imagen = models.ImageField(upload_to='imagenes/', blank=True, null=True)
-
-    def _str_(self):
+    imagen = models.ImageField(upload_to="imagenes/", blank=True, null=True)
+    
+    def __str__(self):
         return self.titulo
--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_public, name='lista_public'),
-    # Si querés podés comentar la galería individual
+    
     # path('galeria/', views.lista_obras, name='lista_obras'),
 ]
